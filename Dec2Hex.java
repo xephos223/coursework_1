9aaa672bba549fbae939daec8e497287a853906a
xephos223 <55843663+xephos223@users.noreply.github.com>
2019-11-04 17:33:36 +0000

    Update Dec2Hex.java
    Cleaned up the formatting

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -20,20 +20,20 @@ class Dec2Hex
             }
         
 
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+			char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+			int rem, num;
+			num = Arg1;
+			String hexadecimal="";
+				System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        while(num != 0)
-        {
-	    rem=num%16;
-	    hexadecimal= ch[rem] + hexadecimal;
+				while(num != 0)
+			{
+			rem=num%16;
+			hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
-        }
+			}
 
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
+			System.out.println("Hexadecimal representation is : " + hexadecimal);
 
 		} 
 		  else 
