7447ade17a89792d8b3eb239688b9f81cf1f715d
xephos223 <55843663+xephos223@users.noreply.github.com>
2019-11-04 17:29:37 +0000

    Update Dec2Hex.java
    Changes made to verifiction

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,11 +7,6 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-      if (args = null)
-      {
-        System.err.println("Please enter a value to be converted.");
-        System.exit(1);
-      }
         if (args.length > 0)
         {
             try
@@ -23,7 +18,7 @@ class Dec2Hex
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
-        }
+        
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
@@ -40,5 +35,12 @@ class Dec2Hex
 
         System.out.println("Hexadecimal representation is : " + hexadecimal);
 
+		} 
+		  else 
+      {
+        System.err.println("Please enter a value to be converetd");
+        System.exit(1);
+      } 
+	  
     }
 }
