043a415df65f250ea7a655a503aabf90645d77b2
xephos223 <55843663+xephos223@users.noreply.github.com>
2019-11-04 17:09:55 +0000

    Slight Correction to verification code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,13 +7,18 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        if (args.length > 0) 
+      if (args = null)
+      {
+        System.err.println("Please enter a value to be converted.");
+        System.exit(1);
+      }
+        if (args.length > 0)
         {
-            try 
+            try
             {
                 Arg1 = Integer.parseInt(args[0]);
-            } 
-            catch (NumberFormatException e) 
+            }
+            catch (NumberFormatException e)
             {
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
@@ -23,16 +28,16 @@ class Dec2Hex
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
 	num = Arg1;
-	String hexadecimal=""; 
+	String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
+
         while(num != 0)
         {
 	    rem=num%16;
 	    hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
-        
+
         System.out.println("Hexadecimal representation is : " + hexadecimal);
 
     }
